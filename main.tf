d8f5feb561ef84d0577ac88ac474a9453467c08b
susanasalazar <76243632+susanasalazar@users.noreply.github.com>
2021-06-02 17:32:03 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "remote" {
-    organization = "hashicorp-learn"    
+    organization = "Data_Hashi"    
     workspaces {
       name = "learn-terraform-pipelines-k8s"
     }
